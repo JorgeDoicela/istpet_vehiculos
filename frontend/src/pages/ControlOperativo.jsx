9b510a702af018c0c0c6f65fd597c911cd7f8c7b
JorgeDoicela <ismael02doicela@gmail.com>
2026-04-14 16:12:12 -0500

    d5

--- frontend/src/pages/ControlOperativo.jsx
+++ frontend/src/pages/ControlOperativo.jsx
@@ -568,7 +568,7 @@ const ControlOperativo = () => {
                         </div>
 
                         {activeTab === 'salida' ? (
-                            <div id="ficha-salida" className="apple-card !pt-9 lg:!pt-12 pb-32 scroll-mt-20">
+                            <div id="ficha-salida" className="apple-card !pt-9 lg:!pt-12 pb-20 lg:pb-10 scroll-mt-20">
                                 <div className="mb-6 px-2 flex items-start justify-between">
                                     <h3 className="text-lg lg:text-2xl font-black text-[var(--apple-text-main)] tracking-tight">Registro de Salida</h3>
                                     <div className="flex flex-col items-end leading-tight">
